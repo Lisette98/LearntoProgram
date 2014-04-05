2994b0ef2a31e0d6814abff7b5fd90294211974d
Lisette98 <lisette.gonzalez98@gmail.com>
2014-04-05 12:39:54 -0400

    Chapter 5 exercise

---
+++ chapter5.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+puts "Hi, what is your first name?"
+
+name=gets.chomp
+
+puts "Great! What is your middle name?"
+
+middle=gets.chomp
+
+puts "Thanks! And what is your last name?"
+
+last=gets.chomp
+
+puts "Pleased to meet you " +name+ " " +middle+ " " +last+ "."
+
+puts "Hi, what is your favorite number?"
+
+num = gets.to_i + 2
+
+puts "Great! But isn't " + num.to_s + " better?"
+
