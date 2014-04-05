a591d6a8ad44ec08230464ec9638ca8932a838d6
Lisette98 <lisette.gonzalez98@gmail.com>
2014-04-05 12:39:19 -0400

    Chapter 2 exercise

---
+++ chapter2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+  puts "How many hours are in a year?"
+    hours = 365 * 24
+  puts "There are " +hours.to_s+ " in a year."
+  
+  puts 
+
+  puts "How many minutes are in a decade?"
+    decades = hours * 60 * 10
+  puts "There are " +decades.to_s+ " minutes in a decade."
+  
+  puts "How many seconds old are you?"
+    age= 39 * 365 * 24 * 60 * 60
+  puts "You are " +age.to_s+ " seconds old."
+  
+  
